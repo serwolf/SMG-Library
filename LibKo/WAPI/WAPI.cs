28e976e4bdaa7dea6507933f685fd2a07932fffd
serwolf <sergiowolf11@yandex.com>
2014-05-29 14:17:24 -0500

    fd

--- LibKo/WAPI/WAPI.cs
+++ LibKo/WAPI/WAPI.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Net.Http;
+using LibKo.ServiceConnection;
 namespace LibKo.WAPI
 {
     public class WAPI
